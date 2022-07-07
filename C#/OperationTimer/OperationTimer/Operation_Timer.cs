4f767d375af8cec9715a23f77318b1f4af6b0271
alkatel3 <74428339+alkatel3@users.noreply.github.com>
2022-07-07 23:13:55 +0300

    OperationTimer

---
+++ C#/OperationTimer/OperationTimer/Operation_Timer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+using static System.Console;
+
+namespace OperationTimer
+{
+    internal sealed class Operation_Timer : IDisposable
+    {
+        long _startTime;
+        string _text;
+        int _collectionCount;
+
+        public Operation_Timer(string text)
+        {
+            PrepareForOperation();
+
+            _text = text;
+            _collectionCount = GC.CollectionCount(0);
+            _startTime = Stopwatch.GetTimestamp();
+
+        }
+
+        public void Dispose()
+        {
+            WriteLine($"{_text}\t{(Stopwatch.GetTimestamp() - _startTime) / (double)Stopwatch.Frequency:0.00} second" +
+                $" (GC {GC.CollectionCount(0) - _collectionCount})");
+        }
+        private static void PrepareForOperation()
+        {
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+        }
+    }
+}
