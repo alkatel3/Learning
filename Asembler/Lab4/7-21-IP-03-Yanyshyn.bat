3f559358b199f83900d817e76efa7343fecd6252
alkatel3 <74428339+alkatel3@users.noreply.github.com>
2022-07-07 14:12:21 +0300

    _

--- Asembler/Lab4/7-21-IP-03-Yanyshyn.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-\masm32\bin\ml /c /coff "7-21-IP-03-Yanyshyn.asm" "7-21-IP-03-Yanyshyn2.asm"
-\masm32\bin\link /subsystem:windows "7-21-IP-03-Yanyshyn.obj" "7-21-IP-03-Yanyshyn2.obj"
-PAUSE
