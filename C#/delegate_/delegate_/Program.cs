862240c042f4721a6b6db19c92eb71ee0cf90ae4
alkatel3 <74428339+alkatel3@users.noreply.github.com>
2022-07-12 00:56:05 +0300

    Work with delegates

---
+++ C#/delegate_/delegate_/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace delegate_
+{
+    class Student
+    {
+        string Name;
+        string SecondName;
+
+        public Student(string name, string secondName)
+        {
+            Name = name;
+            SecondName = secondName;
+        }
+        public override string ToString()
+        {
+            return $"Name: {Name}\t Second Name: {SecondName}";
+        }
+    }
+    class Program
+    {
+        static object FindObj(object[] objs, Predicate<object> del)
+        {
+            object res=null;
+            foreach (var item in objs)
+            {
+                if (del(item))
+                    res = item;
+            }
+            return res;
+        }
+        static bool IsStudent(object obj)
+        {
+            var type = obj.GetType();
+            if (type.Name == "Student")
+            {
+                return true;
+            }
+            return false;
+        }
+        static void Main(string[] args)
+        {
+            object[] objs = 
+            {
+                new Student("Oleg","Yovyk"),
+                14,
+                "Hello",
+                13.123,
+            };
+            Console.WriteLine(FindObj(objs, IsStudent));
+        }
+    }
+}
