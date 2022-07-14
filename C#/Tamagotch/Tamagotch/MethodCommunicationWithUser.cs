d88c9fcf41d1c80cbb9dbaded6d6336edb5300be
alkatel3 <74428339+alkatel3@users.noreply.github.com>
2022-07-14 23:24:35 +0300

    Created class with methods for communication

---
+++ C#/Tamagotch/Tamagotch/MethodCommunicationWithUser.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tamagotch
+{
+    static class MethodCommunicationWithUser
+    {
+        public static void Feed()
+        {
+            MessageBox.Show("Feed me");
+        }
+        public static void Walk()
+        {
+            MessageBox.Show("Walk with me");
+        }
+
+        public static void Sleep()
+        {
+            MessageBox.Show("Put me to sleep");
+        }
+
+        public static void Treat()
+        {
+            MessageBox.Show("Treat me");
+        }
+
+        public static void Play()
+        {
+            MessageBox.Show("Play with me");
+        }
+
+        public static void Die()
+        {
+            if (MessageBox.Show("I died","Game is over", MessageBoxButtons.YesNo)==DialogResult.Yes)
+            {
+                MethodCommunicationWithUser.Play();
+            }
+            
+        }
+    }
+}
