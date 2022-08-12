2cc50322ce6c548396ddf3a475d3f85782809736
alkatel3 <74428339+alkatel3@users.noreply.github.com>
2022-08-13 01:44:49 +0300

    Created class with methods for connection with user

--- C#/Tamagotchi/Tamagotchi/Program.cs
+++ C#/Tamagotchi/Tamagotchi/Program.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Timers;
+using System.Reflection;
+using System.Threading;
 
 namespace Tamagotchi
 {
@@ -11,6 +14,24 @@ namespace Tamagotchi
     {
         static void Main(string[] args)
         {
+            System.Timers.Timer timer = new System.Timers.Timer();
+            timer.Elapsed += MessageToUser.Sleep;
+            timer.Elapsed += MessageToUser.Play;
+            timer.Elapsed += MessageToUser.Walk;
+            timer.Elapsed += MessageToUser.Feed;
+            timer.Interval = 1000;
+            timer.Start();
+            
+            //Type type= typeof(MessageToUser);
+            //MethodInfo[] methods= type.GetMethods();
+            //foreach(MethodInfo method in methods)
+            //{
+            //    if(method.GetParameters().Length == 2)
+            //    {
+            //        timer.Elapsed += method;
+            //    }
+
+            //}
         }
     }
 }
