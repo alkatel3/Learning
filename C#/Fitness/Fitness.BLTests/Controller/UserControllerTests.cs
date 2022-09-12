545796c271fbfd00780a60fa7f542ba65cd1bde7
alkatel3 <74428339+alkatel3@users.noreply.github.com>
2022-09-12 21:26:20 +0300

    Added model testing for user controller

---
+++ C#/Fitness/Fitness.BLTests/Controller/UserControllerTests.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Fitness.BL.Controller;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fitness.BL.Controller.Tests
+{
+    [TestClass()]
+    public class UserControllerTests
+    {
+        [TestMethod()]
+        public void SetNewUserDataTest()
+        {
+            //Arrange
+            var userName = Guid.NewGuid().ToString();
+            var birthDate = DateTime.Now.AddYears(-18);
+            var weight = 90;
+            var height = 190;
+            var gender = "man";
+            var controller = new UserController(userName);
+
+            //Act
+            controller.SetNewUserData(gender, birthDate, weight, height);
+            var controller2 =new UserController(userName);
+
+            //Assert
+            Assert.AreEqual(userName, controller.CurrentUser.Name);
+            Assert.AreEqual(birthDate, controller.CurrentUser.BirthDate);
+            Assert.AreEqual(weight, controller.CurrentUser.Weight);
+            Assert.AreEqual(height, controller.CurrentUser.Height);
+            Assert.AreEqual(gender, controller.CurrentUser.Gender.Name);
+
+        }
+
+        [TestMethod()]
+        public void SaveTest()
+        {
+            //Arrange
+            var userName = Guid.NewGuid().ToString();
+
+            //Act
+            var controller = new UserController(userName);
+
+            //Assert
+            Assert.AreEqual(userName, controller.CurrentUser.Name);
+        }
+    }
+}
