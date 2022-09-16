c4cd2317c4046bb01eb38c637f75899c8ab99ba1
alkatel3 <74428339+alkatel3@users.noreply.github.com>
2022-09-17 00:56:52 +0300

    Created Linked list-based stack

---
+++ C#/DataStructures/MyStack_2/MyStack_2/Model/HardStack.cs
@@ -0,0 +1,60 @@
+﻿
+namespace MyStack_2.Model
+{
+    public class HardStack<T>
+    {
+        private Item<T> Head;
+        public int Count { get; set; }
+        public bool IsEmpty=>Count == 0;
+
+        public HardStack()
+        {
+            Head = null;
+            Count = 0;
+        }
+        public HardStack(T data)
+        {
+            Head = new Item<T> (data);
+            Count = 1;
+        }
+
+        public void Push(T data)
+        {
+            var item = new Item<T>(data);
+            item.Previous = Head;
+            Head = item;
+            Count++;
+        }
+        public T Pop()
+        {
+            if (!IsEmpty)
+            {
+                var item = Head;
+                Head = Head.Previous;
+                Count--;
+                return item.Data;
+
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException("Stack is empty");
+            }
+        }
+        public T Peak()
+        {
+            if (!IsEmpty)
+            {
+                return Head.Data;
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException("Stack is empty");
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Stack with {Count} elements";
+        }
+    }
+}
---
+++ C#/DataStructures/MyStack_2/MyStack_2/Program.cs
@@ -0,0 +1,26 @@
+﻿using MyStack_2.Model;
+
+namespace MyStack_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var Stack = new HardStack<int>();
+            if (!Stack.IsEmpty)
+            {
+                Stack.Pop();
+                Stack.Peak();
+            }
+            for(int i = 0; i < 10; i++)
+            {
+                Stack.Push(i);
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(Stack.Pop());
+            }
+
+        }
+    }
+}
