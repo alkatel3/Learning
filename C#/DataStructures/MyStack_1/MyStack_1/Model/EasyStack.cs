c51ee68ad0853aa895c00ec3149af7dfa8de5d5a
alkatel3 <74428339+alkatel3@users.noreply.github.com>
2022-09-17 00:26:23 +0300

    Created List-based Stack

---
+++ C#/DataStructures/MyStack_1/MyStack_1/Model/EasyStack.cs
@@ -0,0 +1,53 @@
+﻿namespace MyStack_1.Model
+{
+    public class EasyStack<T>:ICloneable
+    {
+        private List<T> Items = new List<T>();
+        public int Count => Items.Count;
+        public bool IsEmpty=>Items.Count == 0;
+
+        public void Push(T item)
+        {
+            Items.Add(item);
+        }
+        public T Pop()
+        {
+            if (!IsEmpty)
+            {
+                var item = Items[Items.Count - 1];
+                Items.RemoveAt(Items.Count - 1);
+                return item;
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+        }
+        public T Peak()
+        {
+            if (!IsEmpty)
+            {
+                return Items[Items.Count - 1];
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+        }
+        public void Clear()
+        {
+            Items.Clear();
+        }
+        public override string ToString()
+        {
+            return $"Stack with {Count} of element";
+        }
+
+        public object Clone()
+        {
+            var newStack=new EasyStack<T>();
+            newStack.Items = new List<T>(Items);
+            return newStack;
+        }
+    }
+}
---
+++ C#/DataStructures/MyStack_1/MyStack_1/Program.cs
@@ -0,0 +1,29 @@
+﻿using MyStack_1.Model;
+
+namespace MyStack_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new EasyStack<int>();
+            if (!stack.IsEmpty)
+            {
+                Console.WriteLine(stack.Pop());
+                Console.WriteLine(stack.Peak());
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                stack.Push(i);
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(stack.Pop());
+            }
+            if (!stack.IsEmpty)
+            {
+                Console.WriteLine(stack.Peak());
+            }
+        }
+    }
+}
