d4685c2de2172e8add0f086b20251f89136322c6
alkatel3 <74428339+alkatel3@users.noreply.github.com>
2022-09-17 01:34:51 +0300

    Created array-based stack

---
+++ C#/DataStructures/MyStack_3/MyStack_3/Model/ArrayStack.cs
@@ -0,0 +1,58 @@
+﻿using System;
+namespace MyStack_3.Model
+{
+    public class ArrayStack<T>
+    {
+        private T[] items;
+        private int current=-1;
+        public bool IsEmpty =>current<0;
+        public int Count => current + 1;
+        public int MaxCount=>items.Length;
+        public ArrayStack(int size=10)
+        {
+            items = new T[size];
+        }
+        public ArrayStack(T data,int size = 10):this(size)
+        {
+            items.Append(data);
+            current++;
+        }
+
+        public void Push(T item)
+        {
+            if (current+1<items.Length)
+            {
+                current++;
+                items[current]=item;
+
+            }
+            else
+                throw new StackOverflowException();
+        }
+        public T Pop()
+        {
+            if (current >= 0)
+            {
+                var item = items[current ];
+                items[current] = default(T);
+                current--;
+                return item;
+            }
+            else
+                throw new ArgumentOutOfRangeException();
+        }
+        public T Peak()
+        {
+            if (current >= 0)
+            {
+                return items[current];
+            }
+            else
+                throw new ArgumentOutOfRangeException();
+        }
+        public override string ToString()
+        {
+            return $"Stack with {current+1} elements";
+        }
+    }
+}
---
+++ C#/DataStructures/MyStack_3/MyStack_3/Program.cs
@@ -0,0 +1,31 @@
+﻿using MyStack_3.Model;
+
+namespace MyStack_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack=new ArrayStack<int>();
+            if (!stack.IsEmpty)
+            {
+                stack.Pop();
+                stack.Peak();
+            }
+            for(int i=0; i < 10; i++)
+            {
+                stack.Push(i);
+                Console.WriteLine();
+                Console.WriteLine($"Count: {stack.Count}");
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(stack.Pop());
+
+                Console.WriteLine();
+                Console.WriteLine($"Count: {stack.Count}");
+            }
+        }
+    }
+}
