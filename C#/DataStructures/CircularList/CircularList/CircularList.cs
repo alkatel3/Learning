6cb0b8ce27df9a778b4578224d66fe1a7547ebe4
alkatel3 <74428339+alkatel3@users.noreply.github.com>
2022-09-19 02:23:04 +0300

    Created circular list

---
+++ C#/DataStructures/CircularList/CircularList/CircularList.cs
@@ -0,0 +1,91 @@
+﻿
+using System.Collections;
+
+namespace CircularList
+{
+    public class CircularList<T>:IEnumerable<T>
+    {
+        private Item<T> Head;
+
+        public int Count { get; private set; } = 0;
+        public bool isEmpty=>Count==0;
+
+        public CircularList()
+        {
+            Head = null;
+        }
+
+        public CircularList(T data)
+        {
+            SetHeadItem(data);
+        }
+
+        private void SetHeadItem(T data)
+        {
+            var item = new Item<T>(data);
+            Head = item;
+            Head.Next = Head;
+            Head.Previos = Head;
+            Count++;
+        }
+
+        public void Add(T data)
+        {
+            if (!isEmpty)
+            {
+                var item=new Item<T>(data);
+                item.Next = Head;
+                item.Previos = Head.Previos;
+                Head.Previos.Next = item ;
+                Head.Previos = item;
+                Count++;
+            }
+            else
+            {
+                SetHeadItem(data);
+                return;
+            }
+        }
+        public void Delete(T data)
+        {
+            if (Head.Data.Equals(data))
+            {
+                RemoveItem(Head);
+                Head = Head.Next;
+                return;
+            }
+            var current = Head;
+            for(int i=Count; i>=0; i--)
+            {
+                if (current!=null && current.Data.Equals(data))
+                {
+                    RemoveItem(current);
+                    return;
+                }
+                current = current.Next;
+            }
+        }
+
+        private void RemoveItem(Item<T> current)
+        {
+            current.Previos.Next = current.Next;
+            current.Next.Previos = current.Previos;
+            Count--;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            var current = Head;
+            for(int i=Count; i > 0; i--)
+            {
+                yield return current.Data;
+                current=current.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ C#/DataStructures/CircularList/CircularList/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace CircularList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var List = new CircularList<int>();
+
+            for(int i = 0; i < 10; i++)
+            {
+                List.Add(i);
+            }
+            foreach(var item in List)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+
+
+            for (int i = 0; i < 10; i++)
+            {
+                List.Add(i);
+            }
+            for (int i = 9; i >= 0; i--)
+            {
+                List.Delete(i);
+            }
+            foreach (var item in List)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+
+
+            for (int i = 0; i < 10; i++)
+            {
+                List.Add(i);
+            }
+            for (int i = 0; i < 10; i += 2)
+            {
+                List.Delete(i);
+            }
+            foreach (var item in List)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    List.Add(i);
+            //}
+            for (int i = 0; i < 10; i++)
+            {
+                List.Delete(i);
+            }
+            foreach (var item in List)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+        }
+    }
+}
