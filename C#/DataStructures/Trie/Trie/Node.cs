a97d81b5eee4870d4ea2767dcaad9d40e1970fe2
alkatel3 <74428339+alkatel3@users.noreply.github.com>
2022-09-21 15:53:06 +0300

    Created trie

---
+++ C#/DataStructures/Trie/Trie/Node.cs
@@ -0,0 +1,54 @@
+﻿
+namespace Trie
+{
+    internal class Node<T>
+    {
+        public char Symbol { get; set; }
+        public T Data { get; set; }
+
+        public bool IsWord { get; set; } 
+
+        public string Prefix { get; set; }
+
+
+
+        public Dictionary<char, Node<T>> SubNodes { get; set; }
+
+        public Node(char symbol, T data, string prefix)
+        {
+            Symbol = symbol;
+            Data = data;
+            Prefix = prefix;
+            SubNodes = new Dictionary<char, Node<T>>();
+        }
+
+        public override string ToString()
+        {
+            return $"{Data} [{SubNodes.Count}] ({Prefix})";
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if(obj is Node<T> node)
+            {
+                return Data.Equals(node);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public Node<T> TryFind(char symbol)
+        {
+            if(SubNodes.TryGetValue(symbol, out Node<T> node))
+            {
+                return node;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ C#/DataStructures/Trie/Trie/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Trie
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var trie = new Trie<int>();
+            trie.Add("привіт", 50);
+            trie.Add("світ", 100);
+            trie.Add("приз", 200);
+            trie.Add("провививка", 50);
+            trie.Add("світовий", 100);
+            trie.Add("подарунок", 200);
+            trie.Add("прапор", 200);
+            trie.Add("рік", 50);
+            trie.Add("ріка", 1000);
+
+            trie.Remove("провививка");
+            trie.Remove("рік");
+
+            Search(trie, "привіт");
+            Search(trie, "провививка");
+            Search(trie, "рік");
+
+        }
+
+        private static void Search(Trie<int> trie, string word)
+        {
+            if (trie.TrySearch(word, out int result))
+            {
+                Console.WriteLine(result);
+            }
+            else
+            {
+                Console.WriteLine("Didn't fint");
+            }
+        }
+    }
+}
