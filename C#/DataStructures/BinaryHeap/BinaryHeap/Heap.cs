50cd3bb9e14819ea149e78282368bd95ffd9a9cc
alkatel3 <74428339+alkatel3@users.noreply.github.com>
2022-09-22 21:33:00 +0300

    Created binary heap

---
+++ C#/DataStructures/BinaryHeap/BinaryHeap/Heap.cs
@@ -0,0 +1,115 @@
+﻿
+using System.Collections;
+
+namespace BinaryHeap
+{
+    public class Heap <T>:IEnumerable<T>
+        where T: IComparable<T>
+    {
+        private List<T>  Items=new List<T> ();
+
+        public int Count => Items.Count;
+
+        public Heap() { }
+
+        public Heap(List<T> items)
+        {
+            Items.AddRange(items);
+            for (int i = Count; i >= 0; i--)
+            {
+                Sort(i);
+            }
+        }
+
+        public T Peak()
+        {
+            if (Count > 0)
+            {
+                return Items[0];
+            }
+            else
+            {
+                return default(T);
+            }
+        }
+
+        public void Add(T item)
+        {
+            Items.Add(item);
+
+            var currentIndex = Count - 1;
+            var parentIngex = GetParentIngex(currentIndex);
+
+            while (currentIndex > 0 && Items[parentIngex].CompareTo(Items[currentIndex]) == -1)
+            {
+                Swap(currentIndex, parentIngex);
+                currentIndex = parentIngex;
+                parentIngex = GetParentIngex(currentIndex);
+            }
+        }
+
+        public T GetMax()
+        {
+            var result = Items[0];
+            Items[0] = Items[Count - 1];
+            Items.RemoveAt(Count - 1);
+
+            Sort(0);
+            return result;
+        }
+
+        private void Sort(int CurrentIndex)
+        {
+            int maxIndex = CurrentIndex;
+            int leftIndex;
+            int rightIndex;
+            while (CurrentIndex < Count)
+            {
+                leftIndex = 2 * CurrentIndex + 1;
+                rightIndex = 2 * CurrentIndex + 2;
+                if(rightIndex<Count&&Items[rightIndex].CompareTo(Items[maxIndex]) == 1)
+                {
+                    maxIndex = rightIndex;
+                }
+                if (leftIndex < Count && Items[leftIndex].CompareTo(Items[maxIndex]) == 1)
+                {
+                    maxIndex = leftIndex;
+                }
+                if (maxIndex == CurrentIndex)
+                {
+                    break;
+                }
+                Swap(CurrentIndex, maxIndex);
+                CurrentIndex = maxIndex;
+            }
+        }
+
+        private void Swap(int currentIndex, int parentIngex)
+        {
+            var temp = Items[currentIndex];
+            Items[currentIndex] = Items[parentIngex];
+            Items[parentIngex] = temp;
+        }
+
+        private static int GetParentIngex(int currentIndex)
+        {
+            return (currentIndex - 1) / 2;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            while (Count > 0)
+            {
+                yield return GetMax();
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            while (Count > 0)
+            {
+                yield return GetMax();
+            }
+        }
+    }
+}
---
+++ C#/DataStructures/BinaryHeap/BinaryHeap/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+
+namespace BinaryHeap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var timer = new Stopwatch();
+            var rnd = new Random();
+            var startItems = new List<int>();
+            for (int i = 0; i < 10000; i++)
+            {
+                startItems.Add(rnd.Next(-1000, 1000));
+            }
+            timer.Start();
+            var heap = new Heap<int>(startItems);
+            timer.Stop();
+            Console.WriteLine(timer.Elapsed);
+            timer.Restart();
+            for (int i = 0; i < 10000; i++)
+            {
+                heap.Add(rnd.Next(-1000, 1000));
+            }
+            timer.Stop();
+            Console.WriteLine(timer.Elapsed);
+            timer.Restart();
+            int j = 1;
+            foreach (var item in heap)
+            {
+                Console.WriteLine($"{j}\t{item}");
+                j++;
+            }
+
+            timer.Stop();
+            Console.WriteLine(timer.Elapsed);
+        }
+    }
+}
