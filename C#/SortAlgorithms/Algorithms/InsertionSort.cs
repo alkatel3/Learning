710117fc430ab00ca2de1b2ec862affc4aaf5410
alkatel3 <74428339+alkatel3@users.noreply.github.com>
2022-10-01 14:46:58 +0300

    Implemented algorithm insertion sort

---
+++ C#/SortAlgorithms/Algorithms/InsertionSort.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public class InsertionSort<T>:AlgorithmBase<T>
+        where T: IComparable<T>
+    {
+        //My realization
+        //
+        //protected override void MakeSort()
+        //{
+        //    int LeftListCount = 1;
+        //    int RightListCount = Items.Count - LeftListCount;
+        //    int insertIndex = LeftListCount;
+
+        //    while (RightListCount > 0)
+        //    {
+        //        for(int i = 0; i < LeftListCount; i++)
+        //        {
+        //            for(int j = insertIndex; j > 0; j--) 
+        //            {
+        //                if (Items[j].CompareTo(Items[j - 1]) == -1)
+        //                {
+        //                    Swop(j, j - 1);
+        //                    ComparisonCount++;
+        //                    continue;
+        //                }
+        //                break;
+        //            }
+        //        }
+        //        LeftListCount++;
+        //        RightListCount--;
+        //        insertIndex++;
+        //    }
+        //}
+
+         protected override void MakeSort()
+         {
+            for(int i = 1; i < Items.Count; i++)
+            {
+                var temp = Items[i];
+                var j = i;
+                while (j > 0 && temp.CompareTo(Items[j - 1]) == -1)
+                {
+                    Items[j] = Items[j - 1];
+                    j--;
+                    SwopCount++;
+                    ComparisonCount++;
+                }
+                Items[j] = temp;
+            }
+        }
+        
+    }
+}
---
+++ C#/SortAlgorithms/AlgorithmsTests/SortTests.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algorithms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Tests
+{
+    [TestClass()]
+    public class SortTests
+    {
+        Random rnd = new Random();
+        List<int> Items = new List<int>();
+        List<int> Sorted = new List<int>();
+
+        [TestInitialize]
+        public void Init()
+        {
+            Items.Clear();
+            for (int i = 0; i < 10000; i++)
+            {
+                Items.Add(rnd.Next(0, 1000));
+            }
+            Sorted.Clear();
+            Sorted.AddRange(Items.OrderBy(x => x).ToArray());
+        }
+
+        [TestMethod()]
+        public void BubbleSortTest()
+        {
+            //arrange
+            var bubble = new BubbleSort<int>();
+            bubble.Items.AddRange(Items);
+            //act
+            bubble.Sort();
+
+            //assert
+            for (int i = 0; i < Items.Count; i++)
+            {
+                Assert.AreEqual(bubble.Items[i], Sorted[i]);
+            }
+        }
+
+        [TestMethod()]
+        public void CocktailSortTest()
+        {
+            //arrange
+            var Cocktail = new CocktailSort<int>();
+            Cocktail.Items.AddRange(Items);
+            //act
+            Cocktail.Sort();
+
+            //assert
+            for (int i = 0; i < Items.Count; i++)
+            {
+                Assert.AreEqual(Cocktail.Items[i], Sorted[i]);
+            }
+        }
+
+        [TestMethod()]
+        public void InsertionSortTest()
+        {
+            //arrange
+            var Insertion = new InsertionSort<int>();
+            Insertion.Items.AddRange(Items);
+            //act
+            Insertion.Sort();
+
+            //assert
+            for (int i = 0; i < Items.Count; i++)
+            {
+                Assert.AreEqual(Insertion.Items[i], Sorted[i]);
+            }
+        }
+    }
+}
